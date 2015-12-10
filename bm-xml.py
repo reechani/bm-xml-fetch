47dba7cc438ce448388d23107ab7e17ef34f31a4
reechani <j.reechani@gmail.com>
2015-12-10 18:04:21 +0100

    Make the script more general and runnable with arguments

--- bm-xml.py
+++ bm-xml.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import sys
+import getopt
 import urllib.request as urllib
 import time
 import json
@@ -8,59 +10,123 @@ from datetime import datetime, timedelta
 # pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
+base_url = 'http://blackmilkclothing.com'
+collection_path = '/collections/all'
 
-links = dict()
 
-phpfile = "bm-cyber-sale-2015.php"
-sale_tag = "Cyber_Sale_2015"
-max_pages = 112
+# TODO:
+# error handlig
+# set baseurl if us
 
-for x in range(1, max_pages+1):
+# once to get nr of pages
+def get_nr_pages(url):
+	# TODO: add error handling
+	# right now it's ok that it's not here, this is not item critical
+	usock = urllib.urlopen(url)
+	html = usock.read()
+	usock.close()
+
+	soup = BeautifulSoup(html)
+	list_soup = soup.select('.pagination-custom li')
+	next_to_last = len(list_soup) - 2
+	last_page = int(list_soup[next_to_last].find('a').text)
+
+	return last_page
+
+
+# for every page to get all items on that page
+def read_collection_page(page, url):
+	# TODO: add error handling
+	url = url + '?page=' + str(page)
+	usock = urllib.urlopen(url)
+	html = usock.read()
+	usock.close()
+
+	soup = BeautifulSoup(html)
+	titles = soup.select(".h6")
+
+	return titles
+
+
+# for every item, pause after each of these
+def check_item_xml_for_tag(url, tag):
+	# TODO: add error handling
+	usock = urllib.urlopen(url + '.xml')
+	xml = usock.read()
+	usock.close()
+
+	xml_soup = BeautifulSoup(xml)
+	tags = xml_soup.find('tags')
+	title = xml_soup.title.text
+
+	if tag in tags.text:
+		# get image
+		img = xml_soup.find('image').find('src').text
+		price = xml_soup.find('price').text
+		# return the item
+		print('found', title)
+		return {'name': title, 'url': url, 'img': img, 'price': price}
+	else:
+		return False
+
+
+def save_to_file(output_file, data):
+	with open(output_file, "w") as fh:
+		json.dump(data, fh)
+	
+
+def main(argv):
+	base_url = 'http://blackmilkclothing.com'
+	tag = None
+	file = None
+	pages = None
+	items = dict()
 	try:
-		# print(x)
-		usock = urllib.urlopen('http://blackmilkclothing.com/collections/all?page=' + str(x))
-		html = usock.read()
-		# print(html)
-		usock.close()
+		opts, args = getopt.getopt(argv, 't:f:', ['us'])
+		# print(opts, args)
+	except getopt.GetoptError:
+		print('nope')
+		exit(2)
+	for opt, arg in opts:
+		if opt == '-t':
+			tag = arg
+			print('tag:', arg)
+		elif opt == '-f':
+			file = arg
+			print('file:', arg)
+		elif opt == '--us':
+			base_url = 'http://us.blackmilkclothing.com'
+			print('us:', 'yes')
+	if tag is None or file is None:
+		print('argument missing')
+		exit(2)
 
-		soup = BeautifulSoup(html)
-		# print(soup)
-		titles = soup.select(".h6"
-	)
-		# print(titles)
+	pages = get_nr_pages(base_url + collection_path)
+	try:
+		for x in range(1, pages+1):
+			print(x)
+			titles = read_collection_page(x, base_url + collection_path)
 
-		for tag in titles:
-			a = tag.find('a')
-			href = a['href']
-			link = "http://blackmilkclothing.com" + href
-			name = a.text
-			# print(name, link)
-
-			usock = urllib.urlopen(link + '.xml')
-			xml = usock.read()
-			usock.close()
-			xml_soup = BeautifulSoup(xml)
-
-			tag = xml_soup.find('tags')
-			img = xml_soup.find('image').find('src').text
-			# print(tag.text)
-			# print(sale_tag in tag.text)
-			if sale_tag in tag.text:
-			    # print(tag.text)
-			    print(name)
-			    links[name] = {'url': link, 'img': img}
-			time.sleep(0.25)
+			for title in titles:
+				a = title.find('a')
+				href = a['href']
+				url = base_url + href
+				# name = a.text
+				item = check_item_xml_for_tag(url, tag)
+				if item != False:
+					# save
+					items[item['name']] = {'url': item['url'], 'img': item['img'], 'price': item['price']}
+					# print(items)
+				# pause!
+				time.sleep(0.15)
 	except socket.error as err:
-		with open(phpfile, "w") as fh:
-			json.dump(links, fh)
+		save_to_file(file, items)
 		print('socket error', err)
 	except urllib.error.URLError as err:
-		with open(phpfile, "w") as fh:
-			json.dump(links, fh)
+		save_to_file(file, items)
 		print('URL error', err)
+	save_to_file(file, items)
 
 
-# print(links)
-
-with open(phpfile, "w") as fh:
-	json.dump(links, fh)
+if __name__ == "__main__":
+	main(sys.argv[1:])
--- main.js
+++ main.js
@@ -1,34 +1,126 @@
-/**
- * Place your JS-code here.
- */
 $(document).ready(function () {
 	'use strict';
 
-	$.ajax({
-		url: 'bm-cyber-sale-2015.php',
-		dataType: 'json',
-		success: function(data){
-			var ordered_keys = Object.keys(data).sort();
-			for (var key in ordered_keys) {
-				var item = data[ordered_keys[key]];
-				var name = ordered_keys[key];
-				var lim_class = "";
-				if(name.indexOf("LIMITED") > -1) {
-					lim_class = "limited";
-				}
-				$('<div>').append(
-					$('<a>').attr('href', item.url).append(
-						$('<img>').attr('src', item.img)
-					)
-				).append(
-					$('<a>').attr('href', item.url).text(name)
-					.addClass('link ' + lim_class)
-				)
-				.addClass('item-img')
-				.appendTo('main');
-			}  
-		}
-	});
+	var old_items;
 
-	console.log('Everything is ready.');
+	var files = {
+/*		'old': 'cyber-2015-old.php', 'new': 'cyber-2015.php'*/
+/*		'old': 'cyber-us-2015-old.php', 'new': 'cyber-us-2015.php'*/
+		'old': 'museum-dec-2015-old.php', 'new': 'museum-dec-2015.php'
+/*		'old': 'museum-dec-us-2015-old.php', 'new': 'museum-dec-us-2015.php'*/
+	};
+
+	var off = 0;
+
+	var title = 'Black Milk Cyber Sale 2015 items';
+	var tag = 'Cyber_Sale_2015';
+
+	var setup = function() {
+		$('<title>').text(title).appendTo($('head'));
+		$('<h1>').text(title).prependTo($('body'));
+		$('#tag').text(tag);
+	};
+
+	var saveData = function(data, into) {
+		switch(into) {
+			case 'old':
+				old_items = data;
+				break;
+		}
+	};
+
+	var get_keys_clean = function(data) {
+		var keys, clean;
+		keys = Object.keys(data).sort();
+		// clean keys
+		clean = keys.map(function(item) {
+			return item.split(' - ')[0].trim();
+		});
+		return clean;
+	};
+
+	var show = function(items, id) {
+		var target, list, li;
+
+		target = $('#' + id);
+
+		list = $('<ul>');
+		for (var key in items) {
+			li = $('<li>').text(items[key]);
+			li.appendTo(list);
+		}
+		list.appendTo(target);
+	};
+
+	var compare = function(new_data, old_data) {
+		var old_keys, new_keys,
+				added = {}, removed = {};
+
+		// get keys
+		old_keys = get_keys_clean(old_data);
+		new_keys = get_keys_clean(new_data);
+
+		// Get diffs
+		added = new_keys.filter(function(item) {
+			return old_keys.indexOf(item) < 0;
+		});
+		removed = old_keys.filter(function(item) {
+			return new_keys.indexOf(item) < 0;
+		});
+
+		// show diffs
+		show(added, 'added');
+		show(removed, 'removed');
+	};
+
+	var get_old = function(file) {
+		$.ajax({
+			url: file,
+			dataType: 'json',
+			success: function(data){
+				saveData(data, 'old');
+			}
+		});
+	};
+
+	var get_new = function(file) {
+		$.ajax({
+			url: file,
+			dataType: 'json',
+			success: function(data){
+				var ordered_keys = Object.keys(data).sort();
+				for (var key in ordered_keys) {
+					var item = data[ordered_keys[key]];
+					var name = ordered_keys[key];
+					var lim_class = "";
+					var price = item.price * (1 - off);
+					if(name.indexOf("LIMITED") > -1) {
+						lim_class = "limited";
+					}
+					$('<div>').append(
+						$('<a>').attr('href', item.url).append(
+							$('<img>').attr('src', item.img)
+						)
+					).append(
+						$('<a>').attr('href', item.url).text(name)
+						.addClass('link ' + lim_class)
+					).append(
+						$('<span>').text('$' + price.toFixed(2))
+					)
+					.addClass('item-img')
+					.appendTo('main');
+				}
+				compare(data, old_items);
+			}
+		});
+	};
+
+	var run = function(files) {
+		setup();
+		get_old(files.old);
+		get_new(files.new);
+	};
+
+	run(files);
+
 });
